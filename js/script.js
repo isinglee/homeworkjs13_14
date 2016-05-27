8f8f639afd8c995065670dbc4c76bc9c0ff31284
Larisa Samoilova <isingle@mail.ru>
2016-05-27 13:48:47 +0300

    Rewrote homeworkjs13_14, added modal window and clean modal.

--- js/script.js
+++ js/script.js
@@ -1,197 +1,94 @@
+'use strict';
 var testProgramming = {
     /**
-    * Массив вопросов с вариантами ответов
-    */
-    questions: [
-        {
-            text: 'Вопрос №$',
-            variants: [
-                {
-                    text: 'Вариант ответа №$'
-                },
-                {
-                    text: 'Вариант ответа №$'
-                },
-                {
-                    text: 'Вариант ответа №$'
-                }
-            ]
-        },
-        {
-            text: 'Вопрос №$',
-            variants: [
-                {
-                    text: 'Вариант ответа №$'
-                },
-                {
-                    text: 'Вариант ответа №$'
-                },
-                {
-                    text: 'Вариант ответа №$'
-                }
-            ]
-        },
-        {
-            text: 'Вопрос №$',
-            variants: [
-                {
-                    text: 'Вариант ответа №$'
-                },
-                {
-                    text: 'Вариант ответа №$'
-                },
-                {
-                    text: 'Вариант ответа №$'
-                }
-            ]
-        }
-    ],
-    /**
-     * Создаёт элемент с тегом what, присваивает ему свойства из properties и вставляет его в элемент по ссылке where
+     * Массив вопросов с вариантами ответов
      */
-    addElement: function(where, what, properties) {
-        // Создаем элемент с заданным тегом (what)
-        var element = document.createElement(what);
-
-        // Проверяем, нужно ли устанавливать созданному элементу какие-то параметры
-        if (typeof properties === 'object') {
-            // Проверяем, нужно ли добавить в объект текстовый узел
-            if (typeof properties.text === 'string') {
-                //Добавляем текст в объект
-                var text = document.createTextNode(properties.text);
-                element.appendChild(text);
-            }
-
-            // Проверяем, содержит ли объект параметры стилей
-            if (typeof properties.style === 'object') {
-                // Проходим по объекту style
-                for (var i in properties.style) {
-                    // Копируем очередной стиль из properties.style в стили элемента
-                    element.style[i] = properties.style[i];
-                }
-            }
-
-            // Проверяем, содержит ли объект классы
-            if (typeof properties.classes === 'object') {
-                // Проходим по массиву classes
-                for (var i in properties.classes) {
-                    // Добавляем очередной класс в список классов элемента
-                    element.classList.add(properties.classes[i]);
-                }
-            }
-
-            // Проверяем, содержит ли объект атрибуты (id, for'' и т.д.) 
-            if (typeof properties.attributes === 'object') {
-                // Проходим по массиву и устанавливаем атрибуты у элемента
-                // Атрибуты заданы в виде объекта, у которого поле это имя атрибута, а значение поля - значение атрибута
-                for (var i in properties.attributes) {
-                    element.setAttribute(i, properties.attributes[i]);
-                }
-            }
-        }
-
-        // Добавляем созданный элемент в конец элемента where
-        where.appendChild(element);
-        
-        // Возвращаем ссылку на созданный элемент, чтобы работать с ним дальше
-        return element;
-    },
-    /**
-     * Создаёт текстовый узел с текстом text и вставляет его в элемент по ссылке where
-     */
-    addText: function(where, text) {
-        // Создаём узел с заданным текстом
-        var textNode = document.createTextNode(text);
-        // Добавляем созданный узел в конец элемента where
-        where.appendChild(textNode);
-        // Возвращаем ссылку на созданный узел, чтобы работать с ним дальше
-        return textNode;
-    },
-    /**
-     * Создаёт форму, наполняет её элементами: заголовком, вопросами и кнопкой отправки и вставляет в корневой элемент по ссылке root
-     */
-    render: function(root) {
-        // Создаём в корневом элементе (root) форму
-        var form = this.addElement(root, 'form');
-        // Добавляем в форму заголовок, список вопросов и подвал с кнопкой отправки
-        this.renderHeader(form);
-        this.renderQuestions(form);
-        this.renderFooter(form);
-    },
-    /**
-     * Создает div с legend внутри
-     */
-    renderHeader: function(root) {
-        // Создаём div, c выравниванием по центру и отступами
-        var div = this.addElement(root, 'div', {
-            style: {
-                'textAlign': 'center',
-                'padding': '10px'
-            }
-        });
-        // Вставляем внутрь элемент legend с текстом заголовка
-        var legend = this.addElement(div, 'legend', {
-            text: 'Тест по программированию'
-        });
-    },
-    renderQuestions: function(root) {
-        // Создаём нумерованный список
-        var ol = this.addElement(root, 'ol');
-
-        // Проходим по массиву вопросов
-        for (var i in this.questions) {
-            // Создаём очередной элемент списка с текстом вопроса
-            var li = this.addElement(ol, 'li', {
-                text: this.questions[i].text.replace('$', +i + 1) // Если в тексте есть "$", заменяем на номер вопроса в массиве +1
-            });
-
-            // Создаём очередной элемент ul, в котором будут варианты ответов
-            var ul = this.addElement(li, 'ul');
-
-            // Проходим по массиву с вариантами ответов
-            for (var j in this.questions[i].variants) {
-                // Создаём очередной элемент списка ответов
-                var li2 = this.addElement(ul, 'li');
-
-                // Добавляем label для чекбокса очередного варианта ответа
-                var label = this.addElement(li2, 'label', {
-                    classes: ['checkbox'],
-                    attributes: {
-                        for: 'checkbox_'+i+'_'+j // Генерируем уникальный ID для чекбокса и label на основе номера вопроса и ответа
-                    }
-                });
-
-                // Добавляем checkbox в label
-                var input = this.addElement(label, 'input', {
-                    attributes: {
-                        type: 'checkbox',
-                        id: 'checkbox_'+i+'_'+j // Генерируем уникальный ID для чекбокса и label на основе номера вопроса и ответа
-                    }
-                });
-
-                // Добавляем текст варианта ответа после checkbox
-                // Если в тексте есть "$", заменяем на номер вопроса в массиве +1
-                var text = this.addText(label, this.questions[i].variants[j].text.replace('$', +j + 1))
-            }
-        }
-    },
-    renderFooter: function(root) {
-        // Создаём div, c выравниванием по центру и отступами
-        var div = this.addElement(root, 'div', {
-            style: {
-                'textAlign': 'center',
-                'padding': '10px'
-            }
-        });
-        // Вставляем внутрь элемент кнопку отправки формы, стилизованную с помощью Bootstrap
-        var button = this.addElement(div, 'button', {
-            text: 'Проверить мои результаты',
-            classes: ['btn', 'btn-primary'],
-            attributes: {
-                type: 'submit'
-            }
-        });
-    }
+    questions: [{
+        text: 'Вопрос №$. Для чего используется конструкция try-catch в javascript?',
+        variants: [{
+            text: '$. Для обработки возможных ошибок.',
+            correct: true
+        }, {
+            text: '$. Для замены условного оператора if.',
+            correct: false
+        }, {
+            text: '$. В строгом режиме весь код необходимо оборачивать в try-catch.',
+            correct: false
+        }]
+    }, {
+        text: 'Вопрос №$. Какой из пунктов верен по отношению к строгому режиму javascript?',
+        variants: [{
+            text: '$. Запрещено дублирование полей объектов.',
+            correct: true
+        }, {
+            text: '$. Запрещено использование директивы eval.',
+            correct: false
+        }, {
+            text: '$. Запрещено удаление полей, имеющих свойство writable = false.',
+            correct: true
+        }]
+    }, {
+        text: 'Вопрос №$. К какому участку скрипта применяется строгие правила "use strict"?',
+        variants: [{
+            text: '$. Во всем скрипте.',
+            correct: false
+        }, {
+            text: '$. Внутри блока.',
+            correct: false
+        }, {
+            text: '$. Либо во всем скрипте, либо в отдельной функции.',
+            correct: true
+        }]
+    }]
 };
+var str = JSON.stringify(testProgramming);
 
-testProgramming.render(document.body);
+localStorage.setItem('testProgramming', str);
+testProgramming = $.parseJSON(localStorage.getItem('testProgramming'));
+
+$(function() {
+    //Генерируем текст из шаблона
+    $('body').append(tmpl('test_template', testProgramming));
+    //Вешаем обработчик на кнопку проверки теста
+    $('#check').click(function(event) {
+        var answerCorrect, i, j,
+            correctAnswers = 0;
+        event.preventDefault();
+        for (var i = 0; i < testProgramming.questions.length; ++i) {
+            answerCorrect = true;
+            for (var j = 0; j < testProgramming.questions[i].variants.length; ++j) {
+                if (
+                    (!testProgramming.questions[i].variants[j].correct &&
+                        $('#checkbox_' + i + '_' + j).prop('checked')
+                    ) ||
+                    (
+                        testProgramming.questions[i].variants[j].correct &&
+                        !$('#checkbox_' + i + '_' + j).prop('checked')
+                    )
+                ) {
+                    // ответ неправильный
+                    answerCorrect = false;
+                }
+            }
+            if (answerCorrect) {
+                ++correctAnswers;
+            }
+        }
+        
+        $('body').append(tmpl('modal_template', {
+            message: "Правильных ответов: " + correctAnswers + " из " + testProgramming.questions.length,
+            button: "Пройти тест заново"
+        }));
+
+        $('.mask, #close').click(function(e) {
+            e.preventDefault();
+            $('.mask, .modal').fadeOut(500, function() {
+                $(this).remove();
+            });
+            $('input[type=checkbox]').prop('checked', false);
+        });
+
+        $('.mask, .modal').fadeIn(500);
+
+    });
+});
